8d602171527c8a5ad80ecf0228b7007915b98e50
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-05-28 18:20:15 +0100

    Array Test Using  (process.argv.slice(2))

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,12 @@
-const Argm = ['No argument','Argument found','Arguments found'];
-for (let i = 0; i<names.length; i++){
-    console.log (names[i]);
+const args = process.argv.slice(2);
+const messages = ['No argument', 'Argument found', 'Arguments found'];
+
+if (args.length === 0) {
+  console.log(messages[0]);
+} else if (args.length === 1) {
+  console.log(messages[1]);
+} else {
+  console.log(messages[2]);
 }
+
 
