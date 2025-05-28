3a38c10a9a3a62ae81b7a806c5fc375461417f55
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-05-28 19:43:10 +0100

    function Expressin

--- 4-concat.js
+++ 4-concat.js
@@ -2,3 +2,11 @@ const speak = function(c){
 console.log ('${c}' is cool);
 };
 speak ('c')
+const speak = function(c){
+console.log ('${c}' is undefined);
+};
+speak ('c')
+const speak = function(c){
+console.log ('${c}' undefined is undefined);
+};
+speak ('c')
